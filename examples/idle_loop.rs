11ec059cb562f0ffe8ce01ca38901364839c5a26
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-03-26 14:47:37 -0400

    add an idle_loop example

---
+++ examples/idle_loop.rs
@@ -0,0 +1,26 @@
+extern crate rustbox;
+extern crate tiny;
+
+use std::borrow::Borrow;
+
+use tiny::tui::{TUI, TUIRet};
+
+fn loop_() -> Option<String> {
+    let mut tui = TUI::new();
+
+    loop {
+        match tui.idle_loop() {
+            TUIRet::SendMsg(cmd) => {
+                tui.show_outgoing_msg(cmd.into_iter().collect::<String>().borrow());
+            },
+            TUIRet::Abort => {
+                return None;
+            },
+            _ => {}
+        }
+    }
+}
+
+fn main() {
+    loop_().map(|err| println!("{}", err));
+}
