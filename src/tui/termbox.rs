76c28ff0ae137a5e25e8f570f901fc776f450bc1
Ömer Sinan Ağacan <omeragacan@gmail.com>
2016-05-10 09:46:43 -0400

    minor: remove redundant enumerate() call

--- src/tui/termbox.rs
+++ src/tui/termbox.rs
@@ -3,9 +3,10 @@
 use termbox_sys;
 
 #[inline]
-pub fn print(pos_x : i32, pos_y : i32, fg : u16, bg : u16, str : &str) {
-    for (char_idx, char) in str.chars().enumerate() {
-        print_char(pos_x + (char_idx as i32), pos_y, fg, bg, char);
+pub fn print(mut pos_x : i32, pos_y : i32, fg : u16, bg : u16, str : &str) {
+    for char in str.chars() {
+        print_char(pos_x, pos_y, fg, bg, char);
+        pos_x += 1;
     }
 }
 
