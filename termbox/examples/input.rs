b6eaf8a71ff9276d1044928677289ccc85b495b5
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-01-15 17:33:03 +0300

    Fix termbox_simple example program

--- termbox/examples/input.rs
+++ termbox/examples/input.rs
@@ -3,27 +3,8 @@ extern crate termbox_simple;
 
 use termbox_simple::*;
 
-use std::thread::sleep;
-use std::time::Duration;
-
-struct Termbox {}
-
-impl Termbox {
-    fn new() -> Termbox {
-        unsafe { tb_init(); }
-        Termbox {}
-    }
-}
-
-impl Drop for Termbox {
-    fn drop(&mut self) {
-        unsafe { tb_shutdown(); }
-        println!("Dropped");
-    }
-}
-
 fn main() {
-    let termbox = Termbox::new();
+    let mut termbox = Termbox::init().unwrap();
 
     // Set up the descriptors for select()
     let mut fd_set : libc::fd_set = unsafe { std::mem::zeroed() };
@@ -42,18 +23,17 @@ fn main() {
             };
 
         if ret == -1 {
-            unsafe { tb_resize(); }
+            termbox.resize();
         } else if unsafe { libc::FD_ISSET(0, &mut fd_set_) } {
             let mut buf : Vec<u8> = vec![];
             if read_input_events(&mut buf) {
                 let string = format!("{:?}", buf);
-                unsafe { tb_clear(); }
+                termbox.clear();
                 if buf == vec![27] { break; }
                 for (char_idx, char) in string.chars().enumerate() {
-                    unsafe { tb_change_cell(char_idx as libc::c_int,
-                                            0, char as u32, TB_WHITE, TB_DEFAULT); }
+                    termbox.change_cell(char_idx as libc::c_int, 0, char, TB_WHITE, TB_DEFAULT);
                 }
-                unsafe { tb_present(); }
+                termbox.present();
             }
         }
     }
