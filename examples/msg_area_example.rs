933090e9d1639735b4e966f45f6a919142b20780
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-05-14 16:26:29 +0300

    Port msg_area_example to ev_loop

--- examples/msg_area_example.rs
+++ examples/msg_area_example.rs
@@ -1,39 +1,25 @@
+extern crate ev_loop;
 extern crate libc;
 extern crate term_input;
 extern crate termbox_simple;
 extern crate time;
 extern crate tiny;
 
+use ev_loop::{EvLoop, READ_EV};
 use term_input::{Input, Event};
-
-use std::mem;
-
 use tiny::tui::{TUI, TUIRet, MsgTarget};
 
-fn loop_() -> Option<String> {
+fn main() {
     let mut tui = TUI::new();
     tui.new_server_tab("debug");
+    tui.draw();
 
-    let mut fd_set : libc::fd_set = unsafe { mem::zeroed() };
-    unsafe { libc::FD_SET(libc::STDIN_FILENO, &mut fd_set); }
-    let nfds = libc::STDIN_FILENO + 1;
+    let mut ev_loop: EvLoop<TUI> = EvLoop::new();
 
-    let mut input = Input::new();
-    let mut ev_buffer : Vec<Event> = Vec::new();
-
-    loop {
-        tui.draw();
-
-        let mut fd_set_ = fd_set.clone();
-        let ret = unsafe {
-            libc::select(nfds,
-                         &mut fd_set_,           // read fds
-                         std::ptr::null_mut(),   // write fds
-                         std::ptr::null_mut(),   // error fds
-                         std::ptr::null_mut())   // timeval
-        };
-
-        if unsafe { ret == -1 || libc::FD_ISSET(0, &mut fd_set_) } {
+    {
+        let mut ev_buffer: Vec<Event> = Vec::new();
+        let mut input = Input::new();
+        ev_loop.add_fd(libc::STDIN_FILENO, READ_EV, Box::new(move |_, ctrl, tui| {
             input.read_input_events(&mut ev_buffer);
             for ev in ev_buffer.drain(0..) {
                 match tui.handle_input_event(ev) {
@@ -43,15 +29,14 @@ fn loop_() -> Option<String> {
                                     &MsgTarget::Server { serv_name: "debug" });
                     },
                     TUIRet::Abort => {
-                        return None;
+                        ctrl.stop();
                     },
                     _ => {}
                 }
             }
-        }
+            tui.draw();
+        }));
     }
-}
 
-fn main() {
-    loop_().map(|err| println!("{}", err));
+    ev_loop.run(tui);
 }
