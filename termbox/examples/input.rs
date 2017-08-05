3a7907129c1c5caef54a4f9e90e62085af9f4ad7
Ömer Sinan Ağacan <omeragacan@gmail.com>
2017-08-05 10:55:24 +0300

    termbox_simple: use term_input's read_stdin() in example program

--- termbox/examples/input.rs
+++ termbox/examples/input.rs
@@ -1,5 +1,6 @@
 extern crate libc;
 extern crate mio;
+extern crate term_input;
 extern crate termbox_simple;
 
 use mio::Events;
@@ -29,7 +30,7 @@ fn main() {
             }
             Ok(_) => {
                 let mut buf : Vec<u8> = vec![];
-                if read_input_events(&mut buf) {
+                if term_input::read_stdin(&mut buf) {
                     let string = format!("{:?}", buf);
                     termbox.clear();
                     if buf == vec![27] {
@@ -42,27 +43,5 @@ fn main() {
                 }
             }
         }
-    }
-}
-
-/// Read stdin contents if it's ready for reading. Returns true when it was able
-/// to read. Buffer is not modified when return value is 0.
-fn read_input_events(buf : &mut Vec<u8>) -> bool {
-    let mut bytes_available : i32 = 0; // this really needs to be a 32-bit value
-    let ioctl_ret = unsafe { libc::ioctl(libc::STDIN_FILENO, libc::FIONREAD, &mut bytes_available) };
-    // println!("ioctl_ret: {}", ioctl_ret);
-    // println!("bytes_available: {}", bytes_available);
-    if ioctl_ret < 0 || bytes_available == 0 {
-        false
-    } else {
-        buf.clear();
-        buf.reserve(bytes_available as usize);
-
-        let buf_ptr : *mut libc::c_void = buf.as_ptr() as *mut libc::c_void;
-        let bytes_read = unsafe { libc::read(libc::STDIN_FILENO, buf_ptr, bytes_available as usize) };
-        debug_assert!(bytes_read == bytes_available as isize);
-
-        unsafe { buf.set_len(bytes_read as usize); }
-        true
     }
 }
