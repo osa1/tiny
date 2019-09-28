3914193129aeb073daca50e798976e9b5434a72e
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-09-28 06:36:02 +0300

    Describing UI API methods

--- libtiny_ui/src/lib.rs
+++ libtiny_ui/src/lib.rs
@@ -1,3 +1,5 @@
+use time::Tm;
+
 /// Target of a message to be shown in a UI.
 pub enum MsgTarget<'a> {
     /// Show the message in the server tab.
@@ -68,4 +70,77 @@ impl MsgSource {
             MsgSource::User { ref nick, .. } => nick,
         }
     }
+}
+
+pub trait UI {
+    /// Create a new server tab.
+    fn new_server_tab(&self, serv: &str);
+
+    /// Close a server tab and all channel and user tabs for that server.
+    fn close_server_tab(&self, serv: &str);
+
+    /// Create a channel tab in the given server.
+    fn new_chan_tab(&self, serv: &str, chan: &str);
+
+    /// Close a channel tab.
+    fn close_chan_tab(&self, serv: &str, chan: &str);
+
+    /// Close a user tab.
+    fn close_user_tab(&self, serv: &str, nick: &str);
+
+    /// Show a message coming from a client in the given tabs.
+    fn add_client_msg(&self, msg: &str, target: &MsgTarget);
+
+    /// Show a plain message with a timestamp in the given tabs.
+    // TODO: What is a "plain message"? Not descriptive.
+    fn add_msg(&self, msg: &str, ts: Tm, target: &MsgTarget);
+
+    /// Show an IRC error message in the given tabs.
+    fn add_err_msg(&self, msg: &str, ts: Tm, target: &MsgTarget);
+
+    /// Show a client error message in the given tabs. Usuallys generated in case of a command
+    /// error, e.g. "unknown command".
+    fn add_client_err_msg(&self, msg: &str, target: &MsgTarget);
+
+    /// Clear all nicks in a server from the UI's cache. Does not show anything.
+    fn clear_nicks(&self, serv: &str);
+
+    /// Set the client's nick in the given server.
+    fn set_nick(&self, serv: &str, nick: &str);
+
+    /// Show a user message sent to the client or to a channel.
+    ///
+    /// - highlight: Whether to highlight the message. Usually set `true` when the message mentions
+    ///   the client's nick.
+    ///
+    /// - is_action: `true` when this is a CTCP ACTION message.
+    ///
+    fn add_privmsg(
+        &self,
+        sender: &str,
+        msg: &str,
+        ts: Tm,
+        target: &MsgTarget,
+        highlight: bool,
+        is_action: bool,
+    );
+
+    /// Add a nick to the given tabs. When `ts` is not provided this does not show anything; just
+    /// updated the channel nick list etc. Otherwise this shows a line like "foo joined channel".
+    fn add_nick(&self, nick: &str, ts: Option<Tm>, target: &MsgTarget);
+
+    /// Remove a nick from given tabs. Similar to `add_nick`, when `ts` is not provided this does
+    /// not show a "foo left channel" line.
+    fn remove_nick(&self, nick: &str, ts: Option<Tm>, target: &MsgTarget);
+
+    /// Rename a nick in the given tabs.
+    fn rename_nick(&self, old_nick: &str, new_nick: &str, ts: Tm, target: &MsgTarget);
+
+    /// Set topic of given tabs.
+    fn set_topic(&self, topic: &str, ts: Tm, serv_name: &str, chan_name: &str);
+
+    /// Do we have a tab for the given user? This is useful for deciding where to show a PRIVMSG
+    /// coming from server; e.g. messages from services sometimes shown in their own tabs,
+    /// sometimes in the server tab.
+    fn user_tab_exists(&self, serv: &str, nick: &str) -> bool;
 }
