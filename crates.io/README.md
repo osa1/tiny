a77cc1c1d52c4cda13c86817a4afc2f665b3c5b6
Ömer Sinan Ağacan <omeragacan@gmail.com>
2019-10-04 22:51:44 +0300

    Create crates.io package for 0.5
    tiny is no longer published on crates.io, just print a note about that
    and refer users to the git repo.
    
    Fixes #140

---
+++ crates.io/README.md
@@ -0,0 +1,6 @@
+Starting with 0.5 tiny is no longer published on crates.io.
+Please install with
+
+    cargo install --git https://github.com/osa1/tiny.git
+
+If you encounter any problems please see README at https://github.com/osa1/tiny
---
+++ crates.io/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("\
+        Starting with 0.5 tiny is no longer published on crates.io.\n\
+        Please install with\n\
+        \n\
+            \tcargo install --git https://github.com/osa1/tiny.git\n\
+        \n\
+        If you encounter any problems please see README at https://github.com/osa1/tiny
+    ");
+}
