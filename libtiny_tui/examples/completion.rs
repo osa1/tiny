92c8337affbca38b315c8eb56e9da67feddf86da
Ömer Sinan Ağacan <omeragacan@gmail.com>
2020-04-11 14:21:21 +0300

    tui: Fix 'completion' example
    (#139)

---
+++ libtiny_tui/examples/completion.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use std::io::Read;
+use std::path::PathBuf;
+
+use libtiny_tui::TUI;
+use libtiny_ui::*;
+
+fn main() {
+    let mut runtime = tokio::runtime::Builder::new()
+        .basic_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let local = tokio::task::LocalSet::new();
+
+    local.block_on(&mut runtime, async move {
+        let (tui, _) = TUI::run(PathBuf::from("../tiny/config.yml"));
+        tui.new_server_tab("debug");
+        let debug_tab = MsgTarget::Server { serv: "debug" };
+
+        tui.add_msg(
+            "Loading word list for auto-completion ...",
+            time::now(),
+            &debug_tab,
+        );
+        tui.draw();
+
+        {
+            let mut contents = String::new();
+            let mut file = File::open("/usr/share/dict/american-english").unwrap();
+            file.read_to_string(&mut contents).unwrap();
+            for word in contents.lines() {
+                tui.add_nick(word, None, &debug_tab);
+            }
+        }
+
+        tui.add_msg("Done.", time::now(), &debug_tab);
+        tui.draw();
+    });
+
+    runtime.block_on(local);
+}
